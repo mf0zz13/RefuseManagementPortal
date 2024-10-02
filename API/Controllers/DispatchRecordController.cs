7fb8c62c320a32ec658035940402b5cc010b4a50
mf0zz13 <michael.foster31@outlook.com>
2024-10-02 15:07:11 -0400

    Updated File structure, and added api files

---
+++ API/Controllers/DispatchRecordController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using Dapper;
+using DispatchRecordAPI.Models;
+
+namespace DispatchRecordAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DispatchRecordController : ControllerBase
+    {
+        [HttpGet]
+        //[Route("AllDispatchRecords")]
+        public List<DispatchRecord> GetAllDispatchRecords()
+        {
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "mysqlmssa.mysql.database.azure.com",
+                Database = "guntherrefuse",
+                UserID = "applogin",
+                Password = "T3stP@ssw0rd",
+                SslMode = MySqlSslMode.Required,
+            };
+
+            var connection = new MySqlConnection(builder.ConnectionString);
+
+            connection.Open();
+
+            return connection.Query<DispatchRecord>("SELECT * FROM DispatchRecords;").ToList();
+        }
+
+        //[HttpGet]
+        //[Route("AllDispatchRecordsFromToday")]
+        //public List<DispatchRecord>? GetDispatchRecordsFromToday()
+        //{
+        //    return connection.Query<DispatchRecord>($"SELECT * FROM DispatchRecords WHERE DispatchDate = {DateTime.Today.Year}-{DateTime.Today.Month}-{DateTime.Today.Day}").ToList();
+        //}
+    }
+}
---
+++ API/Models/DispatchRecord.cs
@@ -0,0 +1,14 @@
+﻿namespace DispatchRecordAPI.Models
+{
+    public class DispatchRecord
+    {
+        public required DateTime Date { get; set; }
+        public required string ServiceArea { get; set; }
+        public required string Route { get; set; }
+        public required string TruckNumber { get; set; }
+        public required string Driver { get; set; }
+        public required string HelperOne { get; set; }
+        public string? HelperTwo { get; set; }
+        public required string RefuseType { get; set; }
+    }
+}
