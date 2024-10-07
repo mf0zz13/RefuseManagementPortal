9745a4581c7f2f687fe7555be58874b96cebed05
mf0zz13 <michael.foster31@outlook.com>
2024-10-07 15:40:02 -0400

    Updated usage of HttpClient to not be redundant.

--- Service/DataBaseConnection.cs
+++ Service/DataBaseConnection.cs
@@ -6,13 +6,12 @@ namespace RefuseManagementPortal.Service
 {
     public class DataBaseConnection
     {
+        private readonly HttpClient client = new()
+        {
+            BaseAddress = new Uri(@"https://guntherrefusedispatchapi.azurewebsites.net/DispatchRecord/")
+        };
         public async Task<List<T>> GetRecordAsync<T>(string endPoint)
         {
-            HttpClient client = new();
-            const string _baseUrl = @"https://guntherrefusedispatchapi.azurewebsites.net/DispatchRecord/";
-
-            client.BaseAddress = new Uri(_baseUrl);
-
             var result = await client.GetFromJsonAsync<List<T>>(endPoint) ?? new List<T>();
             return result;
         }
