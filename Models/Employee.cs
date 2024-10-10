afae594ec999586df1c2a40aee0b98ceb2feab8e
mf0zz13 <michael.foster31@outlook.com>
2024-10-10 00:49:40 -0400

    Added Employee View Model

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace RefuseManagementPortal.Models
+{
+    public class Employee
+    {
+        public string EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+        public DateTime? CdlExpiration { get; set; }
+        public DateTime EmploymentStartDate { get; set; }
+        public DateTime? EmploymentEndDate { get; set; }
+    }
+}
