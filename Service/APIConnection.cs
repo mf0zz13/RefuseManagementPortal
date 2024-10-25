9f80c2200d362fc406865d8256be56c41f356f22
mf0zz13 <michael.foster31@outlook.com>
2024-10-25 11:40:30 -0400

    Removed tests

---
+++ Service/APIConnection.cs
@@ -0,0 +1,21 @@
+﻿using RefuseManagementPortal.Models;
+using System.Net;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+
+namespace RefuseManagementPortal.Service
+{
+    public class APIConnection
+    {
+        private static string uri = "https://guntherrefusedispatchapi.azurewebsites.net/DispatchRecord/";
+        private readonly HttpClient client = new()
+        {
+            BaseAddress = new Uri(uri)
+        };
+        public async Task<List<T>> GetRecordAsync<T>(string endPoint)
+        {
+            var result = await client.GetFromJsonAsync<List<T>>(endPoint) ?? new List<T>();
+            return result;
+        }
+    }
+}
